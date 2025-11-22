6f5c78636c5c8eb17266b55fe0a2915de3112fc0
jeremie1207 <jbarh086@uottawa.ca>
2025-11-22 15:10:26 -0500

    implements the copy button

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const sliderValueText = document.querySelector(".character_length");
 const options = document.querySelectorAll(".option");
 const form = document.querySelector(".password_generator");
 const btnGenerate = document.getElementById("generate");
+const passwordInput = document.getElementById("password");
+const btnCopy = document.querySelector(".btn-copy");
+const copiedText = document.querySelector(".copy_text");
 
 const isAnyOptionsChecked = () =>
   Array.from(options).some((option) => option.checked);
 
+const isPasswordInputEmpty = () => passwordInput.textContent === "";
+
 const isSliderValueZero = () => parseInt(slider.value) !== 0;
 
-const checkValidity = () => {
+const generateBntState = () => {
   btnGenerate.disabled = !(isAnyOptionsChecked() && isSliderValueZero());
+};
+
+const copyBtnState = () => {
+  btnCopy.disabled = !(isPasswordInputEmpty());
 };
 
 slider.addEventListener("input", function () {
@@ -22,8 +31,18 @@ slider.addEventListener("input", function () {
                                         hsl(248, 15%, 11%) 100%)`;
 });
 
-slider.addEventListener("change", checkValidity);
-options.forEach((option) => option.addEventListener("change", checkValidity));
+slider.addEventListener("change", generateBntState);
+options.forEach((option) => option.addEventListener("change", generateBntState));
+passwordInput.addEventListener("change", copyBtnState);
+
+btnCopy.addEventListener("click", async function() {
+  await navigator.clipboard.writeText(passwordInput.value);
+  copiedText.classList.remove("hidden");
+
+  setTimeout(()=> {
+    copiedText.classList.add("hidden");
+  }, 2500);
+});
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
